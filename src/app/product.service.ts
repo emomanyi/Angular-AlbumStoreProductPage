697c9601ddb610bd8ba8e1588228d876c3f42642
atbftr <enockenm@gmail.com>
2021-11-24 09:31:06 +0300

    part5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import { Album } from './album';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/observable';
 @Injectable()
 export class ProductService {
   private _albumUrl:string = '../assets/album.json';
